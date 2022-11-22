e08628a828bee16b9d7240668befeea54f97ac62
Darlan Alves <me@darlanalv.es>
2022-11-22 07:22:59 +0000

    fix: remove unused API and export default CLI

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
-import { CommandLineInterface } from './cli.js';
+import { ProxyManager, Proxy, DomainOption, Certificate } from './proxy-manager.js';
 
-export * from './cli.js';
-export * from './proxy-manager.js';
+const manager = new ProxyManager();
 
-export default new CommandLineInterface();
+export default {
+  addProxy(options: Proxy) {
+    return manager.addProxy(options);
+  },
+
+  removeProxy(options: DomainOption) {
+    return manager.removeProxy(options);
+  },
+
+  addCertificate(options: Certificate) {
+    return manager.addCertificate(options);
+  },
+
+  removeCertificate(options: DomainOption) {
+    return manager.removeCertificate(options);
+  },
+
+  reload() {
+    return manager.reloadProxies();
+  },
+}
--- src/server.ts
+++
@@ -1,3 +0,0 @@
-import px from './index.js';
-
-px.start({ port: +process.env.PORT });
