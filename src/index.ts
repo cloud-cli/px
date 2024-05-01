3d428e4fd9aa7d19fe302b8a4ba287702eeacf4e
Darlan Alves <me@darlanalv.es>
2024-05-01 19:29:47 +0200

    fix: use storage API from Cloudy
    BREAKING CHANGE:
    - Use cloudy storage provider instead of a database for storage portability
    - Domain and Path are combined into one field. Multiple entries can be specified for the same domain, using a different path

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,24 @@
-import { init } from '@cloud-cli/cli';
-import { Resource, SQLiteDriver } from '@cloud-cli/store';
-import { ProxyManager, DomainAndTarget, Domain } from './proxy-manager.js';
-import { loadTargets, Proxy } from './proxy.js';
-import { ProxyServer, ProxySettings, ProxyEntry } from '@cloud-cli/proxy';
+import { init, logInfo } from '@cloud-cli/cli';
+import { ProxyManager, DomainAndTarget, Domain, Proxy } from './proxy-manager.js';
 
-const settings = new ProxySettings({
-  certificatesFolder: process.env.PX_CERTS_FOLDER || '/etc/letsencrypt/live',
-  certificateFile: 'fullchain.pem',
-  keyFile: 'privkey.pem',
-});
-
-const px = new ProxyServer(settings);
+type ExtraProps = { _: string[] };
 const manager = new ProxyManager();
 
-async function reload() {
-  const targets = await loadTargets();
-  px.reset();
-
-  targets.forEach((t) => {
-    px.add(
-      new ProxyEntry({
-        domain: t.domain,
-        target: t.target,
-        path: t.path,
-        redirectToHttps: t.redirect,
-        redirectToUrl: t.redirectUrl,
-        cors: t.cors,
-        headers: t.headers,
-        authorization: t.authorization,
-      }),
-    );
-  });
-
-  px.start();
-}
-
-async function add(options: Proxy) {
+async function add(options: Proxy & ExtraProps) {
   const proxy = await manager.addProxy(options);
-  await reload();
+  await manager.reload();
   return proxy;
 }
 
 async function remove(options: DomainAndTarget) {
   const removed = await manager.removeProxy(options);
-  await reload();
+  await manager.reload();
   return removed;
 }
 
-async function update(options: Proxy) {
+async function update(options: Proxy & ExtraProps) {
   const output = await manager.updateProxy(options);
-  await reload();
+  await manager.reload();
   return output;
 }
 
@@ -66,11 +35,13 @@ function domains() {
 }
 
 async function initServer() {
-  Resource.use(new SQLiteDriver());
-  await Resource.create(Proxy);
-  await reload();
+  await manager.reload();
+  return manager.server;
+}
 
-  return px;
+async function reload() {
+  logInfo('Reloading proxy server');
+  return await manager.reload();
 }
 
 export default { add, remove, list, get, update, domains, reload, [init]: initServer };
--- src/proxy-manager.ts
+++ src/proxy-manager.ts
@@ -1,16 +1,48 @@
-import { Query, Resource } from '@cloud-cli/store';
-import { Proxy } from './proxy.js';
+import { getStorage } from '@cloud-cli/cli';
+import { ProxyEntry, ProxyServer, ProxySettings } from '@cloud-cli/proxy';
 
 const domainNotSpecifiedError = new Error('Domain not specified');
 const targetNotSpecifiedError = new Error('Target not specified');
+const { get, set, remove, getAll } = getStorage<Proxy>('px');
 
-export interface Domain {
-  domain: string;
+const settings = new ProxySettings({
+  certificatesFolder: process.env.PX_CERTS_FOLDER || '/etc/letsencrypt/live',
+  certificateFile: 'fullchain.pem',
+  keyFile: 'privkey.pem',
+});
+
+const px = new ProxyServer(settings);
+const emptyProxy = {
+  domain: '',
+  target: '',
+  cors: false,
+  redirect: false,
+  redirectUrl: '',
+  headers: '',
+  authorization: '',
+};
+
+interface OptionalProps {
   host?: string;
+  _: string[];
+}
+
+export interface Domain extends OptionalProps {
+  domain: string;
 }
 
 export interface DomainAndTarget extends Domain {
   target: string;
+}
+
+export interface Proxy{
+  domain: string;
+  target: string;
+  redirect: boolean;
+  redirectUrl: string;
+  headers: string;
+  authorization: string;
+  cors: boolean;
 }
 
 type KeysOf<T> = {
@@ -22,52 +54,45 @@ type ClassProperties<T> = {
 };
 
 export class ProxyManager {
-  addProxy(proxy: ClassProperties<Proxy> & { host?: string }) {
-    return new Promise(async (resolve, reject) => {
-      if (!proxy.domain && !proxy.host) {
-        return reject(domainNotSpecifiedError);
-      }
+  server = px;
 
-      if (!proxy.target && !proxy.redirectUrl) {
-        return reject(targetNotSpecifiedError);
-      }
+  async addProxy(proxy: ClassProperties<Proxy> & OptionalProps) {
+    if (!proxy.domain && !proxy.host) {
+      throw domainNotSpecifiedError;
+    }
 
-      const [domain, path = ''] = (proxy.domain || proxy.host).split('/');
+    if (!proxy.target && !proxy.redirectUrl) {
+      throw targetNotSpecifiedError;
+    }
 
-      const entry = new Proxy({
-        domain: domain,
-        path: path,
-        target: proxy.target,
-        cors: !!proxy.cors,
-        redirect: !!proxy.redirect,
-        redirectUrl: proxy.redirectUrl || '',
-        headers: proxy.headers || '',
-        authorization: proxy.authorization,
-      });
+    const domainAndPath = proxy.domain || proxy.host || proxy._[0];
+    const entry: Proxy = {
+      domain: domainAndPath,
+      target: proxy.target,
+      cors: !!proxy.cors,
+      redirect: !!proxy.redirect,
+      redirectUrl: proxy.redirectUrl || '',
+      headers: proxy.headers || '',
+      authorization: proxy.authorization,
+    };
 
-      try {
-        const id = await entry.save();
-        entry.id = id;
-        resolve(entry);
-      } catch (error) {
-        reject(error);
-      }
-    });
+    await set(domainAndPath, entry)
+
+    return entry;
   }
 
-  async updateProxy(options: ClassProperties<Proxy> & { host?: string }) {
-    const host = options.domain || options.host;
+  async updateProxy(options: ClassProperties<Proxy> & OptionalProps) {
+    const host = options.domain || options.host || options._[0];
     const proxies = await this.findByDomainAndPath(host);
 
     if (!proxies.length) {
-      const [domain, path = ''] = host.split('/');
-      proxies.push(new Proxy({ domain, path }));
+      proxies.push({ ...emptyProxy, domain: host });
     }
 
     const properties = ['target', 'cors', 'redirect', 'redirectUrl', 'headers', 'authorization'];
     for (const proxy of proxies) {
       properties.forEach((p) => p in options && (proxy[p] = options[p]));
-      await proxy.save();
+      set(proxy.domain, proxy);
     }
 
     return true;
@@ -85,7 +110,7 @@ export class ProxyManager {
 
       try {
         for (const p of proxies) {
-          await p.remove();
+          await remove(p.domain);
         }
         resolve({ found: proxies.length });
       } catch (error) {
@@ -95,29 +120,46 @@ export class ProxyManager {
   }
 
   async getDomainList() {
-    const proxies = await Resource.find(Proxy, new Query<Proxy>());
-    const list = proxies.map((proxy) => proxy.domain);
-    return [...new Set(list)];
+    const proxies = await getAll();
+    return proxies.map((proxy) => proxy.domain);
   }
 
   async getProxyList() {
-    return Resource.find(Proxy, new Query<Proxy>());
+    return await getAll();
   }
 
   getProxyListForDomain(options: Domain) {
     return this.findByDomainAndPath(options.domain || options.host);
   }
 
+  async reload() {
+    const targets = await getAll();
+    px.reset();
+
+    targets.forEach((t) => {
+      const [domain, path = ''] = t.domain.split('/');
+
+      px.add(
+        new ProxyEntry({
+          domain: domain,
+          path: path,
+          target: t.target,
+          redirectToHttps: t.redirect,
+          redirectToUrl: t.redirectUrl,
+          cors: t.cors,
+          headers: t.headers,
+          authorization: t.authorization,
+        }),
+      );
+    });
+
+    px.start();
+  }
+
   private async findByDomainAndPath(string: string) {
     const [domain, path = ''] = string.split('/');
-    const query = new Query<Proxy>().where('domain').is(domain);
+    const proxies = await getAll();
 
-    if (path) {
-      query.where('path').is(path);
-    }
-
-    const proxies = await Resource.find(Proxy, query);
-
-    return proxies;
+    return proxies.filter(p => p.domain === domain && (!path || domain === path));
   }
 }
